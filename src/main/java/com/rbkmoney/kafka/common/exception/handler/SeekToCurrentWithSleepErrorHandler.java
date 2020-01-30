6adcb41ff3c89dca68743866c5ff08455fae9463
Pavel Popov <popoffpavel@yandex.ru>
2020-01-30 11:55:44 +0300

    BJ-780: Add SeekToCurrentWithSleepErrorHandler

---
+++ src/main/java/com/rbkmoney/kafka/common/exception/handler/SeekToCurrentWithSleepErrorHandler.java
@@ -0,0 +1,44 @@
+package com.rbkmoney.kafka.common.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SeekToCurrentWithSleepErrorHandler extends SeekToCurrentErrorHandler {
+
+    private final Integer sleepTimeSeconds;
+
+    public SeekToCurrentWithSleepErrorHandler() {
+        super();
+        this.sleepTimeSeconds = 5;
+    }
+
+    public SeekToCurrentWithSleepErrorHandler(int sleepTimeSeconds, int maxFailures) {
+        super(maxFailures);
+        this.sleepTimeSeconds = sleepTimeSeconds;
+    }
+
+    @Override
+    public void handle(Exception thrownException, List<ConsumerRecord<?, ?>> records, Consumer<?, ?> consumer, MessageListenerContainer container) {
+        log.error("Records commit failed", thrownException);
+
+        sleepBeforeRetry();
+
+        super.handle(thrownException, records, consumer, container);
+    }
+
+    private void sleepBeforeRetry() {
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(sleepTimeSeconds));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
